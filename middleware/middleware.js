385411c6bf36dead9b70be1b38ed5a6acef4f8d5
Ali111q <bbbeat114@gmail.com>
2023-02-07 04:59:11 +0300

    main

---
+++ middleware/middleware.js
@@ -0,0 +1,37 @@
+const { App } = require("../model/model");
+
+module.exports.versionCheck = async (req, res, next) => {
+    const headers = req.headers;
+    let minVersion = await App.max('min_version');
+    let app = await App.findOne({
+        where: {
+            min_version: minVersion
+        }
+    })
+    if (headers['X-App-Version']) {
+        if (Number.parseFloat(headers['X-App-Version']) >= minVersion) {
+            return next();
+        } else {
+            if (headers['X-Platform']) {
+                return res.json({
+                    status: 300,
+                    msg: 'you need to update',
+                    url: app.dataValues.app_store_url
+                });
+            } else {
+                return res.json({
+                    status: 300,
+                    msg: 'you need to update',
+                    url: app.dataValues.google_play_url
+                });
+            }
+
+        }
+    } else {
+        return res.json({
+            status: 301,
+            msg: 'headers error'
+        })
+    }
+
+}
--- model/model.js
+++ model/model.js
@@ -1,9 +1,33 @@
 const Sequelize = require("sequelize");
 const db = require("../database/dtatabase");
 
+// app details 
+module.exports.App = db.define('app', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    current_version: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    min_version: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    google_play_url: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    app_store_url: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: false
+})
 
-
-
+// user table 
 module.exports.User = db.define('user', {
     username: {
         type: Sequelize.STRING,
@@ -20,8 +44,133 @@ module.exports.User = db.define('user', {
     token: {
         type: Sequelize.STRING,
         allowNull: false
+    },
+    register_time: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
     }
 
 }, { timestamps: false });
+
+
+module.exports.Show = db.define('show', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    priority: {
+        type: Sequelize.INTEGER,
+        defaultValue: 1
+    },
+    image: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    trailer: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    publish_time: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+    },
+}, {
+    timestamps: false,
+});
+
+const Tag = db.define('tag', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    priority: {
+        type: Sequelize.INTEGER,
+        defaultValue: 1
+    },
+},
+    { timestamps: false });
+
+
+const Season = db.define('season', {
+    number: {
+        type: Sequelize.INTEGER
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    publish_time: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+    }
+});
+
+const Episode = db.define('episode', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    episode_url: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    publish_time: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+    }
+});
+
+const Ad = db.define('ad', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    video_url: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    website: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    location: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    is_active: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+    },
+    duration: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    priority: {
+        type: Sequelize.INTEGER,
+        defaultValue: false
+    }
+
+}, {
+    timestamps: false
+});
+
+
+
 
 
